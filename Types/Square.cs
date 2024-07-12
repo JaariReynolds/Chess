1b68b8a5bb1aaa05edc477527b73b7d8109f0ad5
Jaari <jaari.rey@gmail.com>
2024-07-12 14:42:14 +1000

    created Square class for every piece and action to implement

---
+++ Types/Square.cs
@@ -0,0 +1,26 @@
+﻿namespace Chess.Types
+{
+    public class Square
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Square(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public string ToAlgebraicNotation()
+        {
+            char file = (char)('a' + X);
+            int rank = 8 - Y;
+            return $"{file}{rank}";
+        }
+
+        public override string ToString()
+        {
+            return ToAlgebraicNotation();
+        }
+    }
+}

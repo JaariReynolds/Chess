cb2ac28f891a6b8e7b9197442b9846b2831f846f
Jaari <jaari.rey@gmail.com>
2024-08-15 17:55:19 +1000

    updated PieceJsonConverter to camelCase

--- Classes/PieceJsonConverter.cs
+++ Classes/PieceJsonConverter.cs
@@ -22,10 +22,10 @@ namespace Chess.Classes
             using var jsonDoc = JsonDocument.ParseValue(ref reader);
             var root = jsonDoc.RootElement;
 
-            string name = root.GetProperty("Name").GetString();
-            TeamColour teamColour = Enum.Parse<TeamColour>(root.GetProperty("TeamColour").GetString());
-            bool hasMoved = root.GetProperty("HasMoved").GetBoolean();
-            Square square = JsonSerializer.Deserialize<Square>(root.GetProperty("Square").GetRawText(), options);
+            string name = root.GetProperty("name").GetString();
+            TeamColour teamColour = Enum.Parse<TeamColour>(root.GetProperty("teamColour").GetString());
+            bool hasMoved = root.GetProperty("hasMoved").GetBoolean();
+            Square square = JsonSerializer.Deserialize<Square>(root.GetProperty("square").GetRawText(), options);
 
             Console.WriteLine($"Deserializing piece: {name}");
 
@@ -48,12 +48,12 @@ namespace Chess.Classes
             Console.WriteLine($"Serializing piece: {value.Name}");
 
             writer.WriteStartObject();
-            writer.WriteString("Name", value.Name);
-            writer.WritePropertyName("Square");
+            writer.WriteString("name", value.Name);
+            writer.WritePropertyName("square");
             JsonSerializer.Serialize(writer, value.Square, options);
-            writer.WriteString("TeamColour", value.TeamColour.ToString());
-            writer.WriteNumber("PieceValue", value.PieceValue);
-            writer.WriteBoolean("HasMoved", value.HasMoved);
+            writer.WriteString("teamColour", value.TeamColour.ToString());
+            writer.WriteNumber("pieceValue", value.PieceValue);
+            writer.WriteBoolean("hasMoved", value.HasMoved);
             writer.WriteEndObject();
         }
     }

cb6e1cc2faf74ad80ccf96f0eaad9f5c32ebce23
Jaari <jaari.rey@gmail.com>
2024-11-20 00:14:19 +1000

    added IEnumerable.Some(..., int count) extension

---
+++ Classes/GeneralUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace ChessLogic.Classes
+{
+    public static class GeneralUtils
+    {
+        public static bool Some<T>(this IEnumerable<T> source, Func<T, bool> predicate, int count)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+            int matches = 0;
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    matches++;
+                    if (matches >= count) return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
